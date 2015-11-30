032ea0381ffd214fbed59f686598e4177fc44912
Ludovit Scholtz <ludko2@gmail.com>
2015-11-30 17:13:04 +0100

    do not show lang menu if only one language is configured

--- src/AsyncWeb/DefaultBlocks/LangMenu.php
+++ src/AsyncWeb/DefaultBlocks/LangMenu.php
@@ -1,10 +1,15 @@
 <?php
 namespace AsyncWeb\DefaultBlocks;
+use AsyncWeb\System\Language;
 
 
 class LangMenu extends \AsyncWeb\Frontend\Block{
 	public static $USE_BLOCK = true;
 	protected function initTemplate(){
+		if(count(Language::$SUPPORTED_LANGUAGES) <= 1){
+			$this->template = '<!-- Only one language is active: not showing the lang menu -->';
+			return;
+		}
 		$menu = \AsyncWeb\Menu\MainMenu::showLangMenu();
 		$this->template = $menu;
 	}
