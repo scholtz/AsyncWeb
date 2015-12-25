fe60bce865d2932f34931e5ab368a19642d8516e
Ludovit Scholtz <ludko2@gmail.com>
2015-12-25 15:46:48 +0100

    update

--- src/AsyncWeb/DefaultBlocks/Msg.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace AsyncWeb\DefaultBlocks;
-
-class Msg extends \AsyncWeb\Frontend\Block{
-	public function initTemplate(){
-		$this->template = \AsyncWeb\Text\Messages::getInstance()->show()." ";
-	}
-}
