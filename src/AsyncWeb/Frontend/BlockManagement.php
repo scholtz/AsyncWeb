746918a47e6a10dc5c7fbc1aa125406163a2cc7f
Ludovit Scholtz <ludko2@gmail.com>
2016-01-25 18:23:48 +0100

    update

--- src/AsyncWeb/Frontend/BlockManagement.php
+++ src/AsyncWeb/Frontend/BlockManagement.php
@@ -13,11 +13,17 @@ class BlockManagement{
 	}
 	public static function renderWeb(){
 		if(BlockManagement::$defaultBlock == null){
-			if(Block::exists("Index")){
-				$def = Block::create("Index");
+			$index = "Index";
+			$url = URLParser::parse();
+			if(isset($url["tmpl"]["Index"])){
+				$index = $url["tmpl"]["Index"];
+			}
+			
+			if(Block::exists($index)){
+				$def = Block::create($index);
 				BlockManagement::setDefaultBlock($def);
 			}else{
-				echo "Please set up the default block in the settings file or create index block!";exit;
+				throw new Exception(Language::get("Please set up the default block in the settings file or create index block!"));
 			}
 		}
 		
