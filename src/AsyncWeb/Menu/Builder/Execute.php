f13932ee4bf694687fe36adfff7455e3287d7fba
Ludovit Scholtz <ludko2@gmail.com>
2016-01-28 15:55:14 +0100

    improve menu creation

---
+++ src/AsyncWeb/Menu/Builder/Execute.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace AsyncWeb\Menu\Builder;
+
+class Execute extends \AsyncWeb\Helpers\ValueObject {}
---
+++ src/AsyncWeb/Menu/Builder/Path.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace AsyncWeb\Menu\Builder;
+
+class PATH extends \AsyncWeb\Helpers\ValueObject {}
