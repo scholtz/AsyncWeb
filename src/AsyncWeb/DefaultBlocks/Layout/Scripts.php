af5586957d5d4e311657965fd4269f38616a7fed
LudovitScholtz <ludko2@gmail.com>
2017-08-16 11:15:24 +0200

    show errors and warnings in scripts block

--- src/AsyncWeb/DefaultBlocks/Layout/Scripts.php
+++ src/AsyncWeb/DefaultBlocks/Layout/Scripts.php
@@ -2,6 +2,18 @@
 namespace AsyncWeb\DefaultBlocks\Layout;
 class Scripts extends \AsyncWeb\Frontend\Block {
     protected function initTemplate() {
-        $this->template = " ";
+		
+		$ret = "";
+		if($msg = \AsyncWeb\Text\Messages::getInstance()->show()){
+			$msg = str_replace(array("\t", "\r", "\n"), "", $msg);
+			$msg = str_replace("\"", "\\\"", $msg);
+			$msg = str_replace("\\\\", "\\", $msg);
+			$ret.= '<script>';
+			$ret.= '$(".messages").append("' . $msg . '");';
+			$ret.= '$("html, body").animate({scrollTop: $(".messages").offset().top}, 500);';
+			$ret.= '</script>';
+		}
+		
+        $this->template = $ret;
     }
 }
