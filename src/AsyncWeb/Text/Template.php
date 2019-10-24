34b22ed56ebc6c852d20c54591f99a61e24632c4
Ludovit Scholtz <ludko2@gmail.com>
2019-10-24 16:33:46 +0200

    allow better use of templates

--- src/AsyncWeb/Text/Template.php
+++ src/AsyncWeb/Text/Template.php
@@ -8,23 +8,44 @@
 namespace AsyncWeb\Text;
 use AsyncWeb\IO\File;
 class Template {
-    public static function exists($name) {
-        try {
-            if($ret = \AsyncWeb\Frontend\Block::exists($name)){
-                return $ret;
+    public static function exists($name,$allowBlock = true) {
+        if($allowBlock){
+            try {
+                if($ret = \AsyncWeb\Frontend\Block::exists($name)){
+                    return $ret;
+                }
             }
-        }
-        catch(\Exception $exc) {
+            catch(\Exception $exc) {
+            }
         }
 
         $path = \AsyncWeb\Frontend\Block::$TEMPLATES_PATH;
         $path = rtrim($path, "/");
         $path = $path . "/" . $name;
+
         $fullpath = File::exists($path, $dbg);
         if($fullpath) return $fullpath;
+        
+        $name = str_replace("_","/",$name);
+        $lang = \AsyncWeb\System\Language::getLang();
+        foreach(\AsyncWeb\Frontend\Block::$TEMPLATE_PATHS as $path=>$weight){
+            $f = $path."/".$name.".$lang.html";
+            if($f){
+                if($fullpath = File::exists($f, $dbg)){
+                    return $fullpath;
+                }
+            }
+            $f = $path."/".$name.".html";
+            if($f){
+                if($fullpath = File::exists($f, $dbg)){
+                    return $fullpath;
+                }
+            }
+        }
+        
         return File::exists("../templates/emailtemplate.html");
     }
-    public static function loadTemplate($name, array $data, $engine = false, $dbg = false) {
+    public static function loadTemplate($name, array $data, $engine = false, $dbg = false,$allowBlock = true) {
         $path = self::exists($name);
         if (!$path) {
             throw new \Exception(\AsyncWeb\System\Language::get("Template %template% not found", array("%template%" => $file)));
